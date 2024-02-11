92b64a51fd559f2f7d88013405b23ad574549fed
Confidence A.E. <confidoekeanya@gmail.com>
2024-02-11 04:37:55 +0100

    Initial commit

---
+++ blog/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+from .models import Comment
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ["post"]
+        labels = {
+            "user_name": "Your Name",
+            "user_email": "Your Email",
+            "text": "Your Comment",
+        }
+        widgets = {
+            "user_name": forms.TextInput(attrs={"class": "form-control"}),
+            "user_email": forms.EmailInput(attrs={"class": "form-control"}),
+            "text": forms.Textarea(attrs={"class": "form-control"}),
+        }
---
+++ blog/views.py
@@ -0,0 +1,88 @@
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from django.views import View
+
+from .models import Post
+from .forms import CommentForm
+
+
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
+
+
+class SinglePostView(View):
+    template_name = "blog/post-detail.html"
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        stored_posts = request.session.get("stored_posts")
+        is_saved_for_later = post.id in stored_posts
+        return render(request, self.template_name, {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": is_saved_for_later,
+        })
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect("post-detail-page", slug)
+
+        return render(request, self.template_name, {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),
+        })
+
+
+class ReadLaterView(View):
+    template_name = "blog/read-later.html"
+
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts", [])
+
+        context = {}
+
+        if len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else:
+            posts = Post.objects.filter(id__in=stored_posts)
+            context["posts"] = posts
+            context["has_posts"] = True
+
+        return render(request, "blog/stored-posts.html", context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts", [])
+        post_id = int(request.POST["post_id"])
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+
+        request.session["stored_posts"] = stored_posts
+        return redirect("/")
